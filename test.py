ca0bf75ff915742968d364879c63c1ac82a451d7
deepanshu0411 <76894950+deepanshu0411@users.noreply.github.com>
2021-01-03 11:20:03 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+# from array import *
+
+# arr = array('i',[2,5,8,4,9])
+# for i in range(5):
+#     arr.remove(arr.index[2])
+
+# print(arr)
+from numpy import *    
+a = array([1,2,3,5])
+print(a)
